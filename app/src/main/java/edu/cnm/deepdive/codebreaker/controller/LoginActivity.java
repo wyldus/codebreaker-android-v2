862c1de007f43a0e93ba6798943028dd6f48839f
dagoberto <berto35572@gmail.com>
2021-07-28 12:03:04 -0600

    Started LogIn activity

---
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/LoginActivity.java
@@ -0,0 +1,32 @@
+package edu.cnm.deepdive.codebreaker.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import edu.cnm.deepdive.codebreaker.R;
+import edu.cnm.deepdive.codebreaker.databinding.ActivityLoginBinding;
+import edu.cnm.deepdive.codebreaker.service.GoogleSignInService;
+
+public class LoginActivity extends AppCompatActivity {
+
+  private static final int LOGIN_REQUEST_CODE = 8191;
+
+  private GoogleSignInService service;
+  private ActivityLoginBinding binding;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    service = GoogleSignInService.getInstance();
+    service.refresh()
+        .subscribe(
+            (account) -> { /* Switch to MainActivity */ },
+            (throwable) -> {
+              binding = ActivityLoginBinding.inflate(getLayoutInflater());
+              binding.signIn.setOnClickListener((v) ->
+                  service.startSignIn(this, LOGIN_REQUEST_CODE));
+              setContentView(binding.getRoot());
+            }
+        );
+  }
+
+}
